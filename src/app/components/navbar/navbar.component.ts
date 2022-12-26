1654fae21dd26cda952c4bcb9c39dcced4cbb7f9
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-12-26 15:55:34 -0300

    create components content so then it will be possible to change on user actions

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,20 +4,22 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  firstLogoSrc = '../assets/argprog.png';
+  navTitleOne = 'Argentina Programa';
+  navTitleTwo = "// #YoProgramo"
+
   onClick() {
     alert('vas a iniciar sesión');
   }
 
-  constructor(private modalService: NgbModal) {
-  }
+  constructor(private modalService: NgbModal) {}
 
   public open(modal: any): void {
     this.modalService.open(modal);
   }
+
+  ngOnInit(): void {}
 }
-
-
-
